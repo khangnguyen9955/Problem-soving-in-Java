1448213cd9577abd3c35b78283909f5730293791
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-12-12 21:54:49 +0700

    StringCompression problem improved

--- ProblemsInBook/StringCompression/src/com/company/Main.java
+++ ProblemsInBook/StringCompression/src/com/company/Main.java
@@ -19,7 +19,44 @@ public class Main {
         return stringBuilder.length() > str.length() ? str : stringBuilder.toString();
 
     }
+
+    // improve this problem a little
+    // instead of create Stringbuilder and then if this stringbuilder length is larger than the original
+    // it is wasteful
+    // now we just check it first
+    // but it will be duplicated code and we have second loop
+    public static String compress(String str){
+        int finalLength = countCompression(str);
+        if(finalLength >= str.length()){
+            System.out.println(str);
+            return str;}
+        StringBuilder stringBuilder = new StringBuilder(finalLength);
+        int cnt =0;
+        for (int i = 0; i < str.length(); i++) {
+            cnt++;
+            if(i+1 >= str.length() || str.charAt(i) != str.charAt(i+1)){
+                stringBuilder.append(str.charAt(i));
+                stringBuilder.append(cnt);
+                cnt=0;
+            }
+        }
+        System.out.println(stringBuilder.toString());
+        return  stringBuilder.toString();
+    }
+    public static int countCompression(String str){
+        int cntCompressed= 0;
+        int cnt =0;
+        for (int i = 0; i < str.length(); i++) {
+            cnt++;
+            if(i+1 >= str.length() || str.charAt(i) != str.charAt(i+1)){
+                cntCompressed += 1 + String.valueOf(cnt).length(); // +1 means a character + valueof(cnt).length means "1" will be 1
+                // "10" will be 2 characters.
+                cnt=0;
+            }
+        }
+        return cntCompressed;
+    }
     public static void main(String[] args) {
-	// write your code here
+        compress("abbbbcccddeef");
     }
 }
