5f96b5b80d55d798de7f3dcf8ec10ea19bcddd53
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2024-05-25 15:58:53 +0700

    new prob

--- MoveZeroes/src/Main.java
+++ MoveZeroes/src/Main.java
@@ -4,16 +4,29 @@ public class Main {
     }
     class Solution {
         public void moveZeroes(int[] nums) {
-            int zeroSize = 0; // length of the zero
-            for (int i = 0; i < nums.length;i++){
-                if (nums[i] == 0)  {
-                    zeroSize++; // increase the size
+//            int zeroSize = 0; // length of the zero
+//            for (int i = 0; i < nums.length;i++){
+//                if (nums[i] == 0)  {
+//                    zeroSize++; // increase the size
+//                }
+//                else if (zeroSize > 0){ // if the size is > 0
+//                    // swap the first 0 with the nums[i]
+//                    nums[i-zeroSize] = nums[i];
+//                    nums[i] = 0;
+//                }
+//            }
+            // approach 2:
+            // use the first loop to get push all the num
+            // use the second loop to append the 0
+            int zeroPointer= 0;
+            for(int i = 0 ; i < nums.length; i++){
+                if(nums[i] != 0) {
+                    nums[zeroPointer++] = nums[i];
                 }
-                else if (zeroSize > 0){ // if the size is > 0
-                    // swap the first 0 with the nums[i]
-                    nums[i-zeroSize] = nums[i];
-                    nums[i] = 0;
-                }
+            }
+
+            for(;zeroPointer < nums.length; zeroPointer++){
+                nums[zeroPointer] = 0;
             }
         }
     }
